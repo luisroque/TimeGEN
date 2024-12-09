4e116371b23fad326648246e06e2b41e01bdcf8e
Luís Roque <luisroque@MacBook-Pro-3.lan>
2024-12-09 17:07:45 +0000

    epoch number back to non-experimental

--- hitgen/model/create_dataset_versions_vae.py
+++ hitgen/model/create_dataset_versions_vae.py
@@ -494,7 +494,7 @@ class CreateTransformedVersionsCVAE:
         kernel_size = trial.suggest_int("kernel_size", 2, 5)
         pooling_mode = trial.suggest_categorical("pooling_mode", ["max", "average"])
         batch_size = trial.suggest_categorical("batch_size", [8, 16, 32])
-        epochs = trial.suggest_int("epochs", 1, 20, step=1)
+        epochs = trial.suggest_int("epochs", 1, 2000, step=100)
         learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-3)
         bi_rnn = trial.suggest_categorical("bi_rnn", [True, False])
         shuffle = trial.suggest_categorical("shuffle", [True, False])
