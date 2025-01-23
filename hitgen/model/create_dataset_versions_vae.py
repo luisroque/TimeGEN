ef1f6ae070e89f25f6b3decc27444eb199f2e858
Luis Roque <luis_roque@live.com>
2025-01-23 23:53:27 +0000

    epochs back to same level

--- hitgen/model/create_dataset_versions_vae.py
+++ hitgen/model/create_dataset_versions_vae.py
@@ -607,7 +607,7 @@ class CreateTransformedVersionsCVAE:
         kernel_size = trial.suggest_int("kernel_size", 2, 5)
         pooling_mode = trial.suggest_categorical("pooling_mode", ["max", "average"])
         batch_size = trial.suggest_categorical("batch_size", [8, 16, 32])
-        epochs = trial.suggest_int("epochs", 1, 5, step=1)
+        epochs = trial.suggest_int("epochs", 1, 2001, step=100)
         learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-3)
         # bi_rnn = trial.suggest_categorical("bi_rnn", [True, False])
         # shuffle = trial.suggest_categorical("shuffle", [True, False])
