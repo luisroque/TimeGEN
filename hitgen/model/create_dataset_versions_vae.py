e5f0a6bb7bc4f2a5eac07aff199b940d52da4a6e
Luis Roque <luis_roque@live.com>
2025-02-27 18:53:36 +0000

    moving back to experiment mode regarding epochs, num of trials and alpha on predict

--- hitgen/model/create_dataset_versions_vae.py
+++ hitgen/model/create_dataset_versions_vae.py
@@ -823,7 +823,7 @@ class CreateTransformedVersionsCVAE:
         kernel_size = trial.suggest_int("kernel_size", 2, 5)
         pooling_mode = trial.suggest_categorical("pooling_mode", ["max", "average"])
         batch_size = trial.suggest_categorical("batch_size", [4, 8, 16, 32])
-        epochs = trial.suggest_int("epochs", 5, 10, step=5)
+        epochs = trial.suggest_int("epochs", 200, 2000, step=100)
         learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-3)
         # bi_rnn = trial.suggest_categorical("bi_rnn", [True, False])
         # forecasting = trial.suggest_categorical("forecasting", [True, False])
@@ -1150,7 +1150,7 @@ class CreateTransformedVersionsCVAE:
                 data_mask_temporalized.temporalized_dyn_features,
             ]
         )
-        alpha = 0  # x times bigger variance
+        alpha = 3  # x times bigger variance
         epsilon = np.random.normal(size=z_mean.shape) * 0.1
         z_augmented = z_mean + np.exp(0.5 * z_log_var) * alpha * epsilon
         generated_data, predictions = cvae.decoder.predict(
@@ -1189,7 +1189,7 @@ class CreateTransformedVersionsCVAE:
                 data_mask_temporalized.temporalized_dyn_features,
             ]
         )
-        alpha = 0  # x times bigger variance
+        alpha = 3  # x times bigger variance
         epsilon = np.random.normal(size=z_mean.shape) * 0.1
         z_augmented = z_mean + np.exp(0.5 * z_log_var) * alpha * epsilon
         generated_data, predictions = cvae.decoder.predict(
