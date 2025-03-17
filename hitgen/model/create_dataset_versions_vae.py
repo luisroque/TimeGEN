correcting how masking was handled for prediction, improve on the architecture to make it more efficient and more expressive (we were collapsing the features with the time dist layers) and adding more hyperparam such as n_blocks

hitgen/model/create_dataset_versions_vae.py
@@ -632,6 +632,8 @@ class CreateTransformedVersionsCVAE:
         kl_weight: float,
         kernel_size: Tuple[int],
         n_hidden: int,
+        time_dist_units: int,
+        n_blocks: int,
         batch_size: int,
         windows_batch_size: int,
         coverage_fraction: float,
@@ -661,6 +663,8 @@ class CreateTransformedVersionsCVAE:
             "kl_weight": kl_weight,
             "kernel_size": kernel_size,
             "n_hidden": n_hidden,
+            "time_dist_units": time_dist_units,
+            "n_blocks": n_blocks,
             "batch_size": batch_size,
             "windows_batch_size": windows_batch_size,
             "coverage_fraction": coverage_fraction,
@@ -756,200 +760,209 @@ class CreateTransformedVersionsCVAE:
         """
         Objective function for Optuna to tune the CVAE hyperparameters, evaluated using the validation set.
         """
-        try:
-            latent_dim = trial.suggest_int("latent_dim", 8, 128, step=8)
-            if self.freq in ["M", "MS"]:
-                window_size = trial.suggest_int("window_size", 6, 24, step=3)
-            elif self.freq in ["Q", "QS"]:
-                window_size = trial.suggest_int("window_size", 8, 16, step=2)
-            elif self.freq in ["Y", "YS"]:
-                window_size = trial.suggest_int("window_size", 4, 8, step=1)
-            else:
-                window_size = trial.suggest_int("window_size", 4, 24, step=1)
+        # try:
+        latent_dim = trial.suggest_int("latent_dim", 8, 128, step=8)
+        if self.freq in ["M", "MS"]:
+            window_size = trial.suggest_int("window_size", 6, 24, step=3)
+        elif self.freq in ["Q", "QS"]:
+            window_size = trial.suggest_int("window_size", 8, 16, step=2)
+        elif self.freq in ["Y", "YS"]:
+            window_size = trial.suggest_int("window_size", 4, 8, step=1)
+        else:
+            window_size = trial.suggest_int("window_size", 4, 24, step=1)
 
-            stride = trial.suggest_int("stride", 1, window_size // 2, step=1)
-            patience = trial.suggest_int("patience", 10, 15, step=1)
-            kl_weight = trial.suggest_float("kl_weight", 0.05, 0.5)
-            n_hidden = trial.suggest_int("n_hidden", 128, 256, step=32)
+        stride = trial.suggest_int("stride", 1, window_size // 2, step=1)
+        patience = trial.suggest_int("patience", 10, 15, step=1)
+        kl_weight = trial.suggest_float("kl_weight", 0.05, 0.5)
+        n_hidden = trial.suggest_int("n_hidden", 128, 256, step=32)
+        time_dist_units = trial.suggest_int("time_dist_units", 16, 32, step=8)
+        n_blocks = trial.suggest_int("n_layers", 1, 3)
 
-            predefined_kernel_sizes = [(2, 2, 1), (1, 1, 1), (2, 1, 1), (4, 2, 1)]
-            valid_kernel_sizes = [
-                ks
-                for ks in predefined_kernel_sizes
-                if all(window_size >= k for k in ks)
-            ]
-            if not valid_kernel_sizes:
-                valid_kernel_sizes.append((1, 1, 1))
+        predefined_kernel_sizes = [
+            (1,),
+            (2,),
+            (2, 1),
+            (1, 1),
+            (2, 2, 1),
+            (4, 2, 1),
+        ]
 
-            kernel_size = tuple(
-                trial.suggest_categorical("kernel_size", valid_kernel_sizes)
-            )
-            batch_size = trial.suggest_int("batch_size", 8, 24, step=8)
-            windows_batch_size = trial.suggest_int("windows_batch_size", 8, 24, step=8)
-            coverage_fraction = trial.suggest_float("coverage_fraction", 0.3, 0.6)
+        valid_kernels = [ks for ks in predefined_kernel_sizes if len(ks) == n_blocks]
+        if not valid_kernels:
+            raise ValueError(f"No kernel_size tuples of length {n_blocks} found!")
+        kernel_size = trial.suggest_categorical("kernel_size", valid_kernels)
 
-            prediction_mode = trial.suggest_categorical(
-                "prediction_mode", ["one_step_ahead", "multi_step_ahead"]
-            )
-            if prediction_mode == "multi_step_ahead":
-                future_steps = window_size
-            else:
-                future_steps = 1
-            epochs = trial.suggest_int("epochs", 100, 500, step=25)
-            learning_rate = trial.suggest_loguniform("learning_rate", 3e-5, 3e-4)
-            noise_scale_init = trial.suggest_float("noise_scale_init", 0.01, 0.5)
+        batch_size = trial.suggest_int("batch_size", 8, 24, step=8)
+        windows_batch_size = trial.suggest_int("windows_batch_size", 8, 24, step=8)
+        coverage_fraction = trial.suggest_float("coverage_fraction", 0.3, 0.6)
 
-            forecasting = True
+        prediction_mode = trial.suggest_categorical(
+            "prediction_mode", ["one_step_ahead", "multi_step_ahead"]
+        )
+        if prediction_mode == "multi_step_ahead":
+            future_steps = window_size
+        else:
+            future_steps = 1
+        epochs = trial.suggest_int("epochs", 100, 500, step=25)
+        learning_rate = trial.suggest_loguniform("learning_rate", 3e-5, 3e-4)
+        noise_scale_init = trial.suggest_float("noise_scale_init", 0.01, 0.5)
 
-            data_mask_temporalized_train = TemporalizeGenerator(
-                data=self.original_train_wide_transf,
-                mask=self.mask_train_wide,
-                dyn_features=self.train_dyn_features,
-                window_size=window_size,
-                stride=stride,
-                batch_size=batch_size,
-                windows_batch_size=windows_batch_size,
-                coverage_mode="partial",
-                coverage_fraction=coverage_fraction,
-                prediction_mode=prediction_mode,
-                future_steps=future_steps,
-            )
+        forecasting = True
 
-            data_mask_temporalized_val = TemporalizeGenerator(
-                data=self.original_val_wide_transf,
-                mask=self.mask_val_wide,
-                dyn_features=self.val_dyn_features,
-                window_size=window_size,
-                stride=stride,
-                batch_size=batch_size,
-                windows_batch_size=windows_batch_size,
-                coverage_mode="systematic",
-                prediction_mode=prediction_mode,
-                future_steps=future_steps,
-            )
+        data_mask_temporalized_train = TemporalizeGenerator(
+            data=self.original_train_wide_transf,
+            mask=self.mask_train_wide,
+            dyn_features=self.train_dyn_features,
+            window_size=window_size,
+            stride=stride,
+            batch_size=batch_size,
+            windows_batch_size=windows_batch_size,
+            coverage_mode="partial",
+            coverage_fraction=coverage_fraction,
+            prediction_mode=prediction_mode,
+            future_steps=future_steps,
+        )
 
-            encoder, decoder = get_CVAE(
-                window_size=window_size,
-                input_dim=1,  # univariate series
-                latent_dim=latent_dim,
-                noise_scale_init=noise_scale_init,
-                kernel_size=kernel_size,
-                forecasting=forecasting,
-                n_hidden=n_hidden,
-            )
+        data_mask_temporalized_val = TemporalizeGenerator(
+            data=self.original_val_wide_transf,
+            mask=self.mask_val_wide,
+            dyn_features=self.val_dyn_features,
+            window_size=window_size,
+            stride=stride,
+            batch_size=batch_size,
+            windows_batch_size=windows_batch_size,
+            coverage_mode="systematic",
+            prediction_mode=prediction_mode,
+            future_steps=future_steps,
+        )
 
-            cvae = CVAE(
-                encoder, decoder, kl_weight_initial=kl_weight, forecasting=forecasting
-            )
-            cvae.compile(
-                optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
-                metrics=[
-                    cvae.total_loss_tracker,
-                    cvae.reconstruction_loss_tracker,
-                    cvae.kl_loss_tracker,
-                ],
-            )
+        encoder, decoder = get_CVAE(
+            window_size=window_size,
+            input_dim=1,  # univariate series
+            latent_dim=latent_dim,
+            pred_dim=future_steps,
+            time_dist_units=time_dist_units,
+            n_blocks=n_blocks,
+            noise_scale_init=noise_scale_init,
+            kernel_size=kernel_size,
+            forecasting=forecasting,
+            n_hidden=n_hidden,
+        )
 
-            es = EarlyStopping(
-                patience=patience,
-                verbose=1,
-                monitor="val_loss",
-                mode="auto",
-                restore_best_weights=True,
-            )
-            reduce_lr = ReduceLROnPlateau(
-                monitor="val_loss",
-                factor=0.2,
-                patience=10,
-                min_lr=1e-6,
-                cooldown=3,
-                verbose=1,
-            )
+        cvae = CVAE(
+            encoder, decoder, kl_weight_initial=kl_weight, forecasting=forecasting
+        )
+        cvae.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
+            metrics=[
+                cvae.total_loss_tracker,
+                cvae.reconstruction_loss_tracker,
+                cvae.kl_loss_tracker,
+            ],
+        )
 
-            history = cvae.fit(
-                x=data_mask_temporalized_train,
-                validation_data=data_mask_temporalized_val,
-                epochs=epochs,
-                batch_size=batch_size,
-                shuffle=False,
-                callbacks=[es, reduce_lr],
-            )
+        es = EarlyStopping(
+            patience=patience,
+            verbose=1,
+            monitor="val_loss",
+            mode="auto",
+            restore_best_weights=True,
+        )
+        reduce_lr = ReduceLROnPlateau(
+            monitor="val_loss",
+            factor=0.2,
+            patience=10,
+            min_lr=1e-6,
+            cooldown=3,
+            verbose=1,
+        )
 
-            val_loss = min(history.history["loss"])
+        history = cvae.fit(
+            x=data_mask_temporalized_train,
+            validation_data=data_mask_temporalized_val,
+            epochs=epochs,
+            batch_size=batch_size,
+            shuffle=False,
+            callbacks=[es, reduce_lr],
+        )
 
-            synthetic_long = self.predict(
-                cvae,
-                gen_data=data_mask_temporalized_val,
-            )
+        val_loss = min(history.history["loss"])
 
-            if self.opt_score == "discriminative_score":
-                score = self.compute_mean_discriminative_score(
-                    unique_ids=synthetic_long["unique_id"].unique(),
-                    original_data=self.original_long,
-                    synthetic_data=synthetic_long,
-                    method="hitgen",
-                    freq=self.freq,
-                    dataset_name=self.dataset_name,
-                    dataset_group=self.dataset_group,
-                    loss=val_loss,
-                    generate_feature_plot=False,
-                    store_score=False,
-                    store_features_synth=False,
-                    split="hypertuning",
-                )
-            elif self.opt_score == "downstream_score":
-                score = compute_downstream_forecast(
-                    unique_ids=synthetic_long["unique_id"].unique(),
-                    original_data=self.original_long,
-                    synthetic_data=synthetic_long,
-                    method="hitgen",
-                    freq=self.freq,
-                    horizon=self.h,
-                    dataset_name=self.dataset_name,
-                    dataset_group=self.dataset_group,
-                    samples=10,
-                    split="hypertuning",
-                )
-            else:
-                score = val_loss
+        synthetic_long = self.predict(
+            cvae,
+            gen_data=data_mask_temporalized_val,
+        )
 
-            if score is None:
-                print("No valid scores computed. Pruning this trial.")
-                raise optuna.exceptions.TrialPruned()
-
-            self.update_best_scores(
+        if self.opt_score == "discriminative_score":
+            score = self.compute_mean_discriminative_score(
+                unique_ids=synthetic_long["unique_id"].unique(),
                 original_data=self.original_long,
                 synthetic_data=synthetic_long,
-                score=score,
-                latent_dim=latent_dim,
-                window_size=window_size,
-                patience=patience,
-                kl_weight=kl_weight,
-                kernel_size=kernel_size,
-                n_hidden=n_hidden,
-                batch_size=batch_size,
-                windows_batch_size=windows_batch_size,
-                coverage_fraction=coverage_fraction,
-                epochs=epochs,
-                learning_rate=learning_rate,
-                forecasting=forecasting,
-                noise_scale_init=noise_scale_init,
-                prediction_mode=prediction_mode,
-                future_steps=future_steps,
+                method="hitgen",
+                freq=self.freq,
+                dataset_name=self.dataset_name,
+                dataset_group=self.dataset_group,
                 loss=val_loss,
-                trial=trial.number,
+                generate_feature_plot=False,
+                store_score=False,
+                store_features_synth=False,
+                split="hypertuning",
             )
+        elif self.opt_score == "downstream_score":
+            score = compute_downstream_forecast(
+                unique_ids=synthetic_long["unique_id"].unique(),
+                original_data=self.original_long,
+                synthetic_data=synthetic_long,
+                method="hitgen",
+                freq=self.freq,
+                horizon=self.h,
+                dataset_name=self.dataset_name,
+                dataset_group=self.dataset_group,
+                samples=10,
+                split="hypertuning",
+            )
+        else:
+            score = val_loss
 
-            # clean GPU memory between trials
-            del cvae, encoder, decoder
-            tf.keras.backend.clear_session()
-            gc.collect()
-
-            return score
-
-        except Exception as e:
-            print(f"Error in trial: {e}")
+        if score is None:
+            print("No valid scores computed. Pruning this trial.")
             raise optuna.exceptions.TrialPruned()
+
+        self.update_best_scores(
+            original_data=self.original_long,
+            synthetic_data=synthetic_long,
+            score=score,
+            latent_dim=latent_dim,
+            window_size=window_size,
+            patience=patience,
+            kl_weight=kl_weight,
+            kernel_size=kernel_size,
+            n_hidden=n_hidden,
+            time_dist_units=time_dist_units,
+            n_blocks=n_blocks,
+            batch_size=batch_size,
+            windows_batch_size=windows_batch_size,
+            coverage_fraction=coverage_fraction,
+            epochs=epochs,
+            learning_rate=learning_rate,
+            forecasting=forecasting,
+            noise_scale_init=noise_scale_init,
+            prediction_mode=prediction_mode,
+            future_steps=future_steps,
+            loss=val_loss,
+            trial=trial.number,
+        )
+
+        # clean GPU memory between trials
+        del cvae, encoder, decoder
+        tf.keras.backend.clear_session()
+        gc.collect()
+
+        return score
+
+        # except Exception as e:
+        #     print(f"Error in trial: {e}")
+        #     raise optuna.exceptions.TrialPruned()
 
     def hyper_tune_and_train(self, n_trials=100):
         """
@@ -1034,6 +1047,8 @@ class CreateTransformedVersionsCVAE:
             window_size=self.best_params["window_size"],
             input_dim=1,  # univariate series
             latent_dim=self.best_params["latent_dim"],
+            pred_dim=self.best_params["future_steps"],
+            time_dist_units=self.best_params["time_dist_units"],
             noise_scale_init=self.best_params["noise_scale_init"],
             kernel_size=self.best_params["kernel_size"],
             forecasting=self.best_params["forecasting"],
@@ -1149,7 +1164,9 @@ class CreateTransformedVersionsCVAE:
         all_meta = []
 
         for i in range(len(generator)):
-            (batch_x, _) = generator[i]  # => ( (batch_data, batch_mask, batch_dyn), _ )
+            (batch_x, targets) = generator[
+                i
+            ]  # => ( (batch_data, batch_mask, batch_dyn), _ )
             batch_pred = cvae.predict_on_batch(batch_x)  # (recon, forecast)
 
             if isinstance(batch_pred, (tuple, list)):

hitgen/model/models.py
@@ -210,7 +210,7 @@ class TemporalizeGenerator(utils.Sequence):
         self.last_batch_metadata = current_meta  # list of (series_idx, start_idx)
 
         return (
-            (batch_data, batch_mask, batch_dyn),
+            (batch_data, batch_mask, batch_dyn, batch_pred_mask),
             (batch_recon_target, batch_recon_mask, batch_pred_target, batch_pred_mask),
         )
 
@@ -321,13 +321,13 @@ class _DecoderOnlyGenerator(keras.utils.Sequence):
         start_idx = self.cum_sizes[index]
         end_idx = self.cum_sizes[index + 1]
 
-        (b_data, b_mask, b_dyn), _ = self.data_gen[index]
+        (b_data, b_mask, b_dyn, b_pred_mask), _ = self.data_gen[index]
         z_batch = self.z_augmented[
             start_idx:end_idx
         ]  # [b_len, window_size, latent_dim]
 
-        # the decoder expects inputs => [latent_input, mask_input, dyn_input]
-        return [z_batch, b_mask, b_dyn]
+        # the decoder expects inputs => [latent_input, mask_input, dyn_input, pred_mask]
+        return [z_batch, b_mask, b_dyn, b_pred_mask]
 
     def on_epoch_end(self):
         self.data_gen.on_epoch_end()
@@ -394,13 +394,15 @@ class CVAE(keras.Model):
         self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
 
     def call(self, inputs, training=False):
-        batch_data, batch_mask, batch_dyn_features = inputs
+        batch_data, batch_mask, batch_dyn_features, pred_mask = inputs
 
         z_mean, z_log_var, z = self.encoder(
             [batch_data, batch_mask, batch_dyn_features]
         )
 
-        pred_reconst, pred_forecast = self.decoder([z, batch_mask, batch_dyn_features])
+        pred_reconst, pred_forecast = self.decoder(
+            [z, batch_mask, batch_dyn_features, pred_mask]
+        )
 
         return pred_reconst, z_mean, z_log_var, pred_forecast
 
@@ -458,13 +460,14 @@ class CVAE(keras.Model):
         data => ((batch_data, batch_mask, batch_dyn), (batch_recon_target, batch_pred_target))
         """
         (
-            (batch_data, batch_mask, batch_dyn),
+            (batch_data, batch_mask, batch_dyn, pred_mask),
             (recon_target, recon_mask, pred_target, pred_mask),
         ) = data
 
         with tf.GradientTape() as tape:
             pred_reconst, z_mean, z_log_var, pred_forecast = self(
-                (batch_data, batch_mask, batch_dyn), training=True
+                (batch_data, batch_mask, batch_dyn, pred_mask),
+                training=True,
             )
 
             total_loss, reconstruction_loss, prediction_loss, kl_loss = (
@@ -497,7 +500,7 @@ class CVAE(keras.Model):
 
     @tf.function
     def test_step(self, data):
-        (batch_data, batch_mask, batch_dyn), (
+        (batch_data, batch_mask, batch_dyn, pred_mask), (
             recon_target,
             recon_mask,
             pred_target,
@@ -505,7 +508,8 @@ class CVAE(keras.Model):
         ) = data
 
         pred_reconst, z_mean, z_log_var, pred_forecast = self(
-            (batch_data, batch_mask, batch_dyn), training=False
+            (batch_data, batch_mask, batch_dyn, pred_mask),
+            training=False,
         )
         total_loss, reconstruction_loss, prediction_loss, kl_loss = self.compute_loss(
             recon_true=recon_target,
@@ -555,6 +559,9 @@ def get_CVAE(
     window_size: int,
     input_dim: int,
     latent_dim: int,
+    pred_dim: int,
+    time_dist_units: int = 16,
+    n_blocks: int = 3,
     noise_scale_init: float = 0.01,
     kernel_size: Tuple[int] = (2, 2, 1),
     n_hidden: int = 256,
@@ -568,11 +575,14 @@ def get_CVAE(
     input_shape_main = (window_size, input_dim)
     # 6 dynamic features => (window_size, 6)
     input_shape_dyn_features = (window_size, 6)
+    output_shape_pred = (pred_dim, input_dim)
 
     enc = encoder(
         input_shape=input_shape_main,
         input_shape_dyn_features=input_shape_dyn_features,
         latent_dim=latent_dim,
+        time_dist_units=time_dist_units,
+        n_blocks=n_blocks,
         noise_scale_init=noise_scale_init,
         kernel_size=kernel_size,
         n_hidden=n_hidden,
@@ -582,6 +592,9 @@ def get_CVAE(
         output_shape=input_shape_main,
         output_shape_dyn_features=input_shape_dyn_features,
         latent_dim=latent_dim,
+        time_dist_units=time_dist_units,
+        n_blocks=n_blocks,
+        pred_shape=output_shape_pred,
         kernel_size=kernel_size,
         forecasting=forecasting,
         n_hidden=n_hidden,
@@ -634,25 +647,23 @@ class UpsampleTimeLayer(tf.keras.layers.Layer):
         self.method = method
 
     def call(self, x):
-        # x shape: [B, current_len, num_features]
-        x = tf.expand_dims(x, axis=1)  # => [B, 1, current_len, num_features]
+        # x shape: [B, current_len, 1]
+        x = tf.expand_dims(x, axis=1)  # => [B, 1, current_len, 1]
         x_upsampled = tf.image.resize(
             images=x, size=(1, self.target_len), method=self.method
-        )  # => [B, 1, target_len, num_features]
-        x_upsampled = tf.squeeze(
-            x_upsampled, axis=1
-        )  # => [B, target_len, num_features]
+        )  # => [B, 1, target_len, 1]
+        x_upsampled = tf.squeeze(x_upsampled, axis=1)  # => [B, target_len, 1]
         return x_upsampled
 
 
-class MRHIBlock_backcast_forecast(tf.keras.layers.Layer):
+class MRHIBlock_backcast(tf.keras.layers.Layer):
     """
     Block following with MLP projection and upsampling, with added regularization.
     """
 
     def __init__(
         self,
-        backcast_size,
+        backcast_size: Tuple[int, int],
         n_knots: int,
         n_hidden: int = 256,
         n_layers: int = 2,
@@ -669,11 +680,79 @@ class MRHIBlock_backcast_forecast(tf.keras.layers.Layer):
         self.n_layers = n_layers
 
         self.n_theta_backcast = self.seq_len * self.num_features
+        self.n_theta = self.n_theta_backcast
+
+        self.pooling_layers = []
+        for k in kernel_size:
+            self.pooling_layers.append(
+                layers.MaxPooling1D(pool_size=k, strides=k, padding="valid")
+            )
+
+        mlp_layers = [layers.Flatten()]
+        for _ in range(n_layers):
+            mlp_layers.append(
+                layers.Dense(
+                    n_hidden,
+                    kernel_regularizer=regularizers.l2(l2_lambda),
+                    use_bias=False,
+                )
+            )
+            mlp_layers.append(layers.BatchNormalization())
+            mlp_layers.append(layers.Activation(activation))
+            mlp_layers.append(layers.Dropout(dropout_rate))
+
+        mlp_layers.append(
+            layers.Dense(self.n_theta, kernel_regularizer=regularizers.l2(l2_lambda))
+        )
+        self.mlp_stack = tf.keras.Sequential(mlp_layers)
+
+    def call(self, x: tf.Tensor) -> (tf.Tensor, tf.Tensor):
+        """
+        Forward pass with pooling and MLP.
+        """
+        for pool_layer in self.pooling_layers:
+            x = pool_layer(x)
+
+        theta = self.mlp_stack(x)
+
+        backcast_flat = theta
+
+        backcast = tf.reshape(backcast_flat, [-1, self.seq_len, self.num_features])
+
+        return backcast
+
+
+class MRHIBlock_backcast_forecast(tf.keras.layers.Layer):
+    """
+    Block following with MLP projection and upsampling, with added regularization.
+    """
+
+    def __init__(
+        self,
+        backcast_size: Tuple[int, int],
+        forecast_size: Tuple[int, int],
+        n_knots: int,
+        n_hidden: int = 256,
+        n_layers: int = 2,
+        kernel_size: Tuple[int] = (2, 2, 1),
+        activation="relu",
+        dropout_rate: float = 0.2,
+        l2_lambda: float = 1e-4,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.seq_len, self.num_features = backcast_size
+        self.pred_len, self.num_features = forecast_size
+        self.n_knots = n_knots
+        self.n_hidden = n_hidden
+        self.n_layers = n_layers
+
+        self.n_theta_backcast = self.seq_len * self.num_features
         self.n_theta_forecast = self.n_knots * self.num_features
         self.n_theta = self.n_theta_backcast + self.n_theta_forecast
 
         self.upsample_layer = UpsampleTimeLayer(
-            target_len=self.seq_len, method="bilinear"
+            target_len=self.pred_len, method="bilinear"
         )
 
         self.pooling_layers = []
@@ -723,6 +802,7 @@ def encoder(
     input_shape: Tuple[int, int],
     input_shape_dyn_features: Tuple[int, int],
     latent_dim: int,
+    time_dist_units: int = 16,
     n_blocks: int = 3,
     n_hidden: int = 256,
     n_layers: int = 2,
@@ -739,14 +819,14 @@ def encoder(
     masked_input = layers.Concatenate()([dyn_features_input, masked_input])
 
     masked_input = layers.TimeDistributed(
-        layers.Dense(input_shape[1], activation="relu")
+        layers.Dense(time_dist_units, activation="relu")
     )(masked_input)
 
     backcast_total = masked_input
     final_output = 0
 
     for i in range(n_blocks):
-        mrhi_block = MRHIBlock_backcast_forecast(
+        mrhi_block = MRHIBlock_backcast(
             backcast_size=input_shape,
             n_knots=4,
             n_hidden=n_hidden,
@@ -754,7 +834,7 @@ def encoder(
             kernel_size=kernel_size,
         )
 
-        backcast, _ = mrhi_block(backcast_total)
+        backcast = mrhi_block(backcast_total)
 
         backcast_total = backcast_total - backcast
         final_output += backcast
@@ -785,6 +865,8 @@ def decoder(
     output_shape: Tuple[int, int],
     output_shape_dyn_features: Tuple[int, int],
     latent_dim: int,
+    pred_shape: Tuple[int, int],
+    time_dist_units: int = 16,
     n_blocks: int = 3,
     n_hidden: int = 256,
     n_layers: int = 2,
@@ -792,21 +874,17 @@ def decoder(
     forecasting: bool = True,
 ):
     time_steps = output_shape[0]
-    num_features = output_shape[1]
 
     latent_input = layers.Input(shape=(time_steps, latent_dim), name="latent_input")
     dyn_features_input = layers.Input(
         shape=output_shape_dyn_features, name="dyn_features_input"
     )
     mask_input = layers.Input(shape=output_shape, name="mask_input")
+    pred_mask = layers.Input(shape=pred_shape, name="pred_mask")
 
-    x = layers.TimeDistributed(layers.Dense(num_features, activation="relu"))(
-        latent_input
-    )
+    x = layers.Concatenate()([dyn_features_input, latent_input])
 
-    x = layers.Concatenate()([dyn_features_input, x])
-
-    x = layers.TimeDistributed(layers.Dense(num_features, activation="relu"))(x)
+    x = layers.TimeDistributed(layers.Dense(time_dist_units, activation="relu"))(x)
 
     residual = x
     final_forecast = layers.Lambda(lambda t: tf.zeros_like(t))(x)
@@ -816,6 +894,7 @@ def decoder(
         for i in range(n_blocks):
             mrhi_block = MRHIBlock_backcast_forecast(
                 backcast_size=output_shape,
+                forecast_size=pred_shape,
                 n_knots=4,
                 n_hidden=n_hidden,
                 n_layers=n_layers,
@@ -845,10 +924,10 @@ def decoder(
     )
 
     final_forecast = layers.Multiply(name="masked_forecast_output")(
-        [final_forecast, mask_input]
+        [final_forecast, pred_mask]
     )
     return tf.keras.Model(
-        inputs=[latent_input, mask_input, dyn_features_input],
+        inputs=[latent_input, mask_input, dyn_features_input, pred_mask],
         outputs=[final_backcast, final_forecast],
         name="decoder",
     )