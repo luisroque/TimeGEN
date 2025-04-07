fce26f7991c7e4fd6a4c6d8b5baa80b99124a527
Luís Roque <luis_roque@live.com>
2025-04-07 22:56:40 +0100

    run script removing tf implementation

--- hitgen/experiments/testing_HiTGen.py
+++ hitgen/experiments/testing_HiTGen.py
@@ -52,26 +52,9 @@ if __name__ == "__main__":
             FREQ = extract_frequency(subgroup)
             H = extract_horizon(subgroup)
             DATASET_GROUP = subgroup[0]
-            if has_final_score_in_tuple(subgroup):
-                print(
-                    f"Dataset: {DATASET}, Dataset-group: {DATASET_GROUP}, Frequency: {FREQ} "
-                    f"has a final score already, skipping HiTGen scores computations..."
-                )
-                hitgen_score_disc = extract_score(subgroup)
 
             print(
                 f"Dataset: {DATASET}, Dataset-group: {DATASET_GROUP}, Frequency: {FREQ}"
-            )
-
-            print(
-                f"\n\nOptimization score to use for hyptertuning: {args.opt_score}\n\n"
-            )
-
-            SYNTHETIC_FILE_PATH_HITGEN = (
-                f"assets/model_weights/{DATASET}_{DATASET_GROUP}_synthetic_hitgen.pkl"
-            )
-            SYNTHETIC_FILE_PATH_TIMEGAN = (
-                f"assets/model_weights/{DATASET}_{DATASET_GROUP}_synthetic_timegan.pkl"
             )
 
             hitgen_pipeline = HiTGenPipeline(
@@ -87,24 +70,6 @@ if __name__ == "__main__":
             test_unique_ids = hitgen_pipeline.original_test_long["unique_id"].unique()
 
             if not args.transfer_learning:
-                model_forecasting = hitgen_pipeline.hyper_tune_and_train_forecasting()
-
-                row_hitgen = {}
-
-                evaluation_pipeline_hitgen_forecast(
-                    dataset=DATASET,
-                    dataset_group=DATASET_GROUP,
-                    model=model_forecasting,
-                    pipeline=hitgen_pipeline,
-                    horizon=H,
-                    freq=FREQ,
-                    row_forecast=row_hitgen,
-                )
-
-                dataset_group_results.append(row_hitgen)
-                results.append(row_hitgen)
-
-                ######### Benchmark #########
 
                 benchmark_pipeline.hyper_tune_and_train(max_evals=20)
 
@@ -142,32 +107,6 @@ if __name__ == "__main__":
                             horizon=H_TL,
                             opt_score=args.opt_score,
                         )
-
-                        model_forecasting_tl = (
-                            hitgen_pipeline_transfer_learning.hyper_tune_and_train_forecasting()
-                        )
-
-                        row_hitgen_tl = {}
-
-                        evaluation_pipeline_hitgen_forecast(
-                            dataset=DATASET,
-                            dataset_group=DATASET_GROUP,
-                            model=model_forecasting_tl,
-                            pipeline=hitgen_pipeline,
-                            horizon=H,
-                            freq=FREQ,
-                            row_forecast=row_hitgen_tl,
-                            dataset_source=DATASET_TL,
-                            window_size=hitgen_pipeline_transfer_learning.best_params_forecasting[
-                                "window_size"
-                            ],
-                            prediction_mode="out_domain",
-                        )
-
-                        dataset_group_results.append(row_hitgen_tl)
-                        results.append(row_hitgen_tl)
-
-                        ######### Benchmark #########
 
                         benchmark_pipeline_transfer_learning = BenchmarkPipeline(
                             hitgen_pipeline=hitgen_pipeline_transfer_learning
