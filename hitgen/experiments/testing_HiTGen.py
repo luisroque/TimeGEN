bec875ab8ea7fca78ec41068b09bf0bc591b1cf8
Luís Roque <luis_roque@live.com>
2025-04-08 14:11:15 +0100

    adding new datasets

--- hitgen/experiments/testing_HiTGen.py
+++ hitgen/experiments/testing_HiTGen.py
@@ -16,18 +16,32 @@ from hitgen.experiments.helper import (
 
 
 DATASET_GROUP_FREQ = {
-    "Tourism": {
-        "Monthly": {"FREQ": "M", "H": 24},
-    },
-    "M1": {
-        "Monthly": {"FREQ": "M", "H": 24},
-        #     # "Quarterly": {"FREQ": "Q", "H": 8},
-    },
+    # "Tourism": {
+    #     "Monthly": {"FREQ": "M", "H": 24},
+    # },
+    # "M1": {
+    #     "Monthly": {"FREQ": "M", "H": 24},
+    #     "Quarterly": {"FREQ": "Q", "H": 8},
+    # },
     # "M3": {
     #     "Monthly": {"FREQ": "M", "H": 24},
     #     "Quarterly": {"FREQ": "Q", "H": 8},
     #     "Yearly": {"FREQ": "Y", "H": 4},
     # },
+    # "Labour": {
+    #     "Monthly": {"FREQ": "M", "H": 24},
+    # },
+    # "Traffic": {
+    #     "Daily": {"FREQ": "D", "H": 30},
+    # },
+    # "ETTh1": {
+    #     "Daily": {"FREQ": "D", "H": 30},
+    # },
+    "M4": {
+        "Monthly": {"FREQ": "M", "H": 24},
+        #     "Quarterly": {"FREQ": "Q", "H": 8},
+        #     "Yearly": {"FREQ": "Y", "H": 4},
+    },
 }
 
 SOURCE_DATASET_GROUP_FREQ_TRANSFER_LEARNING = {
